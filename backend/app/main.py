62c072c39bae6986bc15b147e0dd471ceea3f134
Shilov_DO <shilovdaniil2004@ya.ru>
2026-06-10 21:49:40 +0300

    Версия для предзащиты 1.2

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,14 +21,23 @@ origins = [
 # Кастомная middleware
 app.middleware("http")(auth_middleware)
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-    allow_headers=["Authorization", "Content-Type", "Accept"],
-    expose_headers=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["Set-Cookie", "Authorization"]
 )
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+#     allow_headers=["Authorization", "Content-Type", "Accept"],
+#     expose_headers=["*"],
+# )
 
 # Подключаем роутеры
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
